b165ee623ae238362401f8f3eb812eaab9e91197
ksey <ksey3000@gmail.com>
2022-04-18 17:35:54 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -28,13 +28,13 @@ if [ "$RKN_ENABLED" = "true" ]; then
     wget --no-verbose --no-check-certificate --user-agent="$USER_AGENT" --output-document=/tmp/upd/rkn.txt --tries=3 $RKN_SOURCE
     if [ $? -eq 0 ]; then
         RKN_SIZE=$(wc -l rkn.txt | cut -d ' ' -f 1)
-		if [ $RKN_SIZE -ge 100 ]; then
+        if [ $RKN_SIZE -ge 100 ]; then
             echo "$(date): Roskomnadzor's unblocking list contains $RKN_SIZE domains. Updating rkn.action..."
-			echo "{+forward-override{forward-socks5 $FORWARD_ADDRESS_PORT .}}" > $CONFIG_PATH/rkn.action
-			cat /tmp/upd/rkn.txt | grep -E "^[0-9A-Za-z]" | sed 's/^/./' >> $CONFIG_PATH/rkn.action
-		else
-		    echo "$(date): Roskomnadzor's unblocking list contains $RKN_SIZE lines. It is too small. Update terminated...."
-		fi
+            echo "{+forward-override{forward-socks5 $FORWARD_ADDRESS_PORT .}}" > $CONFIG_PATH/rkn.action
+            cat /tmp/upd/rkn.txt | grep -E "^[0-9A-Za-z]" | sed 's/^/./' >> $CONFIG_PATH/rkn.action
+        else
+            echo "$(date): Roskomnadzor's unblocking list contains $RKN_SIZE lines. It is too small. Update terminated...."
+        fi
     else
         echo "$(date): Update Roskomnadzor's unblocking list failed. Check update source url: $RKN_SOURCE"
     fi
@@ -46,13 +46,13 @@ if [ "$RU_ENABLED" = "true" ]; then
     wget --no-verbose --no-check-certificate --user-agent="$USER_AGENT" --output-document=/tmp/upd/ru.txt --tries=3 $RU_SOURCE
     if [ $? -eq 0 ]; then
         RU_SIZE=$(wc -l ru.txt | cut -d ' ' -f 1)
-		if [ $RU_SIZE -ge 3 ]; then
+        if [ $RU_SIZE -ge 3 ]; then
             echo "$(date): Russia's unblocking list contains $RU_SIZE domains. Updating ru.action..."
-			echo "{+forward-override{forward-socks5 $FORWARD_ADDRESS_PORT .}}" > $CONFIG_PATH/ru.action
-			cat /tmp/upd/ru.txt | grep -E "^[0-9A-Za-z]" | sed 's/^/./' >> $CONFIG_PATH/ru.action
-		else
-		    echo "$(date): Russia's unblocking list contains $RU_SIZE lines. It is too small. Update terminated...."
-		fi
+            echo "{+forward-override{forward-socks5 $FORWARD_ADDRESS_PORT .}}" > $CONFIG_PATH/ru.action
+            cat /tmp/upd/ru.txt | grep -E "^[0-9A-Za-z]" | sed 's/^/./' >> $CONFIG_PATH/ru.action
+        else
+            echo "$(date): Russia's unblocking list contains $RU_SIZE lines. It is too small. Update terminated...."
+        fi
     else
         echo "$(date): Update Russia's unblocking list failed. Check update source url: $RU_SOURCE"
     fi
